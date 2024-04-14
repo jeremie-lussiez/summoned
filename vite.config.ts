67d182f3a12d13480934e3bc43bb3a1e57b44d58
Jeremie Lussiez <jeremie.lussiez@gmail.com>
2024-04-15 00:47:00 +0200

    fix: ludum dare embedding

--- vite.config.ts
+++ vite.config.ts
@@ -3,47 +3,43 @@ import topLevelAwait from "vite-plugin-top-level-await";
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    // build: {
-    // minify: 'esbuild',
-    // target: "esnext",
-    // rollupOptions: {
-    //     output: {
-    //         format: "iife",
-    //         inlineDynamicImports: true,
-    //     },
-    // }
-    //     rollupOptions: {
-    //         output: {
-    //             inlineDynamicImports: true
-    //         }
-    //     }
-    // },
-    optimizeDeps: {
-        exclude: [
-            "@dimforge/rapier2d/rapier_wasm2d_bg.wasm"
-        ]
-    },
-    plugins: [
-        wasm(),
-        // topLevelAwait()
-    ],
-    worker: {
-        // Not needed with vite-plugin-top-level-await >= 1.3.0
-        // format: "es",
-        // inlineDynamicImports: true,
-        rollupOptions: {
-            output: {
-                inlineDynamicImports: true
-            }
-        },
+    build: {
+        // minify: 'esbuild',
+        // target: "esnext",
+        // chunkSizeWarningLimit: 1000000000000000,
+        // rollupOptions: {
+        //     output: {
+        //         format: "iife",
+        //         manualChunks: () => "index",
+        //         inlineDynamicImports: false,
+        //     },
+        // }
         // optimizeDeps: {
         //     exclude: [
         //         "@dimforge/rapier2d/rapier_wasm2d_bg.wasm"
         //     ]
-        // },
-        plugins: [
-            // wasm(),
-            // topLevelAwait()
-        ]
-    }
+    },
+    // plugins: [
+    // wasm(),
+    // topLevelAwait()
+    // ],
+    // worker: {
+    // Not needed with vite-plugin-top-level-await >= 1.3.0
+    // format: "es",
+    // inlineDynamicImports: true,
+    // rollupOptions: {
+    //     output: {
+    //         inlineDynamicImports: true
+    //     }
+    // },
+    // optimizeDeps: {
+    //     exclude: [
+    //         "@dimforge/rapier2d/rapier_wasm2d_bg.wasm"
+    //     ]
+    // },
+    // plugins: [
+    //     // wasm(),
+    //     // topLevelAwait()
+    // ]
+    // }
 });
